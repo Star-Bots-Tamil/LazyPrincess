9ed35639a670c9a1efdb145d647d24d7eea1e16a
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-09-09 19:43:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -80,7 +80,7 @@ URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', 'd03a53149bf1
 LZURL_PRIME_USERS = [int(lazyurlers) if id_pattern.search(lazyurlers) else lazyurlers for lazyurlers in environ.get('LZURL_PRIME_USERS', '5965340120').split()]
 lazy_groups = environ.get('LAZY_GROUPS','')
 LAZY_GROUPS = [int(lazy_groups) for lazy_groups in lazy_groups.split()] if lazy_groups else None # ADD GROUP ID IN THIS VARIABLE
-my_users = [int(my_users) if id_pattern.search(my_users) else my_users for my_users in environ.get('MY_USERS', '').split()]
+my_users = [int(my_users) if id_pattern.search(my_users) else my_users for my_users in environ.get('MY_USERS', '1391556668 5162208212 5239847373').split()]
 MY_USERS = (my_users) if my_users else []
 BOT_USERNAME = environ.get('BOT_USERNAME' ,'Star_Moviess_Bot' )
 
