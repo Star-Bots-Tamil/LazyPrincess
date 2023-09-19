8ced97d67b3fa3be7f081621bffa1b2519338917
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-09-19 18:59:24 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -719,11 +719,11 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 quote=True,
                 disable_web_page_preview=True,
                 reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Back", callback_data="groupcb")]])
-            )
-        except Exception as e:
-            print(e)  # print the error message in logs
-            await query.message.edit(f"**Error of Your Shortlink URL or API ●>** `{e}`")
-            return
+        )
+    except Exception as e:
+        print(e)  # print the error message in logs
+        await query.message.edit(f"**Error of Your Shortlink URL or API ●>** `{e}`")
+        return
     elif query.data == "pages":
         await query.answer()
     elif query.data == "start":
