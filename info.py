70849bb64ce959daaa12fd0adecc132dd2d7b730
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-11-06 15:00:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -85,7 +85,7 @@ MY_USERS = (my_users) if my_users else []
 BOT_USERNAME = environ.get('BOT_USERNAME' ,'Star_Moviess_Bot' )
 
 # Online Stream and Download
-PORT = int(environ.get('PORT', 8181))
+PORT = int(environ.get('PORT', 8080))
 NO_PORT = bool(environ.get('NO_PORT', False))
 APP_NAME = None
 if 'DYNO' in environ:
@@ -93,7 +93,7 @@ if 'DYNO' in environ:
     APP_NAME = environ.get('APP_NAME')
 else:
     ON_HEROKU = False
-BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '146.190.120.122'))
+BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'star-movies-bot-o372.onrender.com'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
 URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
     "http://{}:{}/".format(FQDN, PORT)
