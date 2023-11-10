35a9717fe86f4858ec48f4f670237d5241728519
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-11-10 08:23:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -93,7 +93,7 @@ if 'DYNO' in environ:
     APP_NAME = environ.get('APP_NAME')
 else:
     ON_HEROKU = False
-BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'star-movies-bot-o372.onrender.com'))
+BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'starmovies.onrender.com'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
 URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
     "http://{}:{}/".format(FQDN, PORT)
