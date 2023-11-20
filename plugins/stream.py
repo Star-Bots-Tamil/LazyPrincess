61ecab232e607abb57c8d437206b72f3cfd875ba
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-11-20 20:08:49 +0530

    Update stream.py

--- plugins/stream.py
+++ plugins/stream.py
@@ -77,35 +77,35 @@ async def private_receive_handler(c: Client, m: Message):
     ),
     group=4,
 )
-async def channel_receive_handler(bot, broadcast):
-    if int(broadcast.chat.id) in BANNED_CHANNELS:
-        await bot.leave_chat(broadcast.chat.id)
+async def channel_receive_handler(c: Client, m: Message bot, broadcast ):
+    if int(m.chat.id) in BANNED_CHANNELS:
+        await c.leave_chat(m.chat.id)
         return
     try:
-        log_msg = await broadcast.forward(chat_id=FILES_CHANNEL)
-        file_name = get_media_file_name(broadcast)
+        log_msg = await m.forward(chat_id=FILES_CHANNEL)
+        file_name = get_media_file_name(m)
         file_hash = get_hash(log_msg, HASH_LENGTH)
         stream_link = "{URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}" if ON_HEROKU or NO_PORT else \
             "{URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
-        shortened_link = await get_channel_shortlink(message.chat.id, stream_link)
+        shortened_link = await get_channel_shortlink(m.chat.id, stream_link)
 
         await log_msg.reply_text(
-            text=f"<b>Channel Name :- {broadcast.chat.title}\nChannel ID :- <code>{broadcast.chat.id}</code>\nRequest URL :- {stream_link}\nShortener Link :- {shortened_link}</b>",
+            text=f"<b>Channel Name :- {m.chat.title}\nChannel ID :- <code>{m.chat.id}</code>\nRequest URL :- {stream_link}\nShortener Link :- {shortened_link}</b>",
             quote=True,
             parse_mode=ParseMode.HTML
         )
-        await message.edit_reply_markup(
-            chat_id=broadcast.chat.id,
-            message_id=broadcast.id,
+        await m.edit_reply_markup(
+            chat_id=m.chat.id,
+            message_id=m.id,
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("📥 Fast Download Link", url=shortened_link)]])
         )
     except FloodWait as w:
         print(f"Sleeping for {str(w.x)}s")
         await asyncio.sleep(w.x)
-        await bot.send_message(chat_id=FILES_CHANNEL,
-                             text=f"<b>Got FloodWait of {str(w.x)}s From {broadcast.chat.title}\n\nChannel ID :-</b> <code>{str(broadcast.chat.id)}</code>",
+        await c.send_message(chat_id=FILES_CHANNEL,
+                             text=f"<b>Got FloodWait of {str(w.x)}s From {m.chat.title}\n\nChannel ID :-</b> <code>{str(m.chat.id)}</code>",
                              disable_web_page_preview=True, parse_mode=ParseMode.HTML)
     except Exception as e:
-        await bot.send_message(chat_id=FILES_CHANNEL, text=f"<b>#Error_Trackback :-</b> <code>{e}</code>", disable_web_page_preview=True, parse_mode=ParseMode.HTML)
+        await c.send_message(chat_id=FILES_CHANNEL, text=f"<b>#Error_Trackback :-</b> <code>{e}</code>", disable_web_page_preview=True, parse_mode=ParseMode.HTML)
         print(f"Can't Edit Broadcast Message!\nError :- {e}")
