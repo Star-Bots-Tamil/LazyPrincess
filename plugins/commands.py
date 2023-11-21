f6d27424519cb238d4cc19d0807fc76db4504bb8
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-11-21 14:34:15 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -48,7 +48,7 @@ def get_shortlinkk(url):
   
 async def get_shortlink(link):
     url = 'https://tnshort.net/api'
-    params = {'api': d03a53149bf186ac74d58ff80d916f7a79ae5745, 'url': link}
+    params = {'api': URL_SHORTNER_WEBSITE_API, 'url': link}
 
     async with aiohttp.ClientSession() as session:
         async with session.get(url, params=params, raise_for_status=True) as response:
